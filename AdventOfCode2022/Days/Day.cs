db64a103910dc2d5a5a8dda123af220b4f560d18
kianf <kianf@DESKTOP-DFNTABQ>
2022-12-01 00:43:34 +0000

    Add project files.

---
+++ AdventOfCode2022/Days/Day.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Days
+{
+    abstract public class Day
+    {
+        public abstract int DayNumber { get; set; }
+
+        protected string InputPath { get; set; }
+
+        public Day(int number)
+        {
+            DayNumber = number;
+            InputPath = Assembly.GetEntryAssembly().Location + $@"\Inputs\Day{DayNumber}.txt";
+        }
+
+        abstract public void Part1();
+        abstract public void Part2();
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode2022.Days;
+
+while (true)
+{
+    Console.WriteLine("Enter the day number you want to run (1-25) or type 'exit' to quit.");
+
+    string input = Console.ReadLine();
+
+    if (input == "exit")
+        break;
+
+    if (int.TryParse(input, out int dayNumber))
+    {
+        if (dayNumber < 1 || dayNumber > 25)
+        {
+            Console.WriteLine("Invalid day number. Please enter a number between 1 and 25.");
+        }
+        else
+        {
+            var type = Type.GetType($"AdventOfCode2022.Days.Day{dayNumber}");
+
+            if (type is null)
+            {
+                Console.WriteLine("Day not implemented yet.");
+            }
+            else
+            {
+                var instance = Activator.CreateInstance(type, dayNumber);
+                if (instance is null) return;
+                
+                var dayToRun = (Day)instance;
+                dayToRun.Part1();
+                dayToRun.Part2();
+            }
+        }
+    }
+    else
+    { 
+        Console.WriteLine("Invalid input. Please enter a number between 1 and 25.");
+    }
+}
